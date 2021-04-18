a84a13d2a4407995525c0fb5ebc324042d78eeea
mukilan.raja.kesavan <mukilan.raja.kesavan@mukilanrajakesavans-MacBook-Pro.local>
2021-04-19 01:01:19 +0530

    [Mukilan] Add. Checked whether if both points are same while calculating distance and directions and return 0 if true

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,5 +1,7 @@
 package org.oop;
 
+import java.util.Objects;
+
 public class Point {
     private final double x;
     private final double y;
@@ -10,6 +12,10 @@ public class Point {
     }
 
     public double distance(Point to) {
+        if(this.equals(to))
+        {
+            return 0;
+        }
         double xDistance = to.x-this.x;
         double yDistance = to.y-this.y;
         double xSquare = Math.pow(xDistance, 2);
@@ -18,8 +24,25 @@ public class Point {
     }
 
     public double direction(Point to) {
+        if(this.equals(to))
+        {
+            return 0;
+        }
         double xDistance = to.x-this.x;
         double yDistance = to.y-this.y;
         return Math.atan2(yDistance, xDistance);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
