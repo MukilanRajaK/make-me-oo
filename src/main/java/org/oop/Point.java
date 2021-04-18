0f8c3b355b6020c8d1cb09fe780a0923f28eff59
mukilan.raja.kesavan <mukilan.raja.kesavan@mukilanrajakesavans-MacBook-Pro.local>
2021-04-19 00:46:46 +0530

    [Mukilan] Refractor. CoOrdinates in Point Class are made final

--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -1,9 +1,9 @@
 package org.oop;
 
 public class Point {
-    private double x;
-    private double y;
-    
+    private final double x;
+    private final double y;
+
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
